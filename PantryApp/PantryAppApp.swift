89e00cff5fffb3d2025a6f86553535cedff0d94a
Brendan Sick <brendansick@gmail.com>
2024-08-30 22:25:38 -0700

    Initial Commit

---
+++ PantryApp/PantryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryAppApp.swift
+//  PantryApp
+//
+//  Created by Brendan Sick on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PantryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
